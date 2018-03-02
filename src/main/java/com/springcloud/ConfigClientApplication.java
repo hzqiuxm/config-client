e2e5526d72dea1cdade9cf313222c0cf0598a163
qiuxm <qiuxm@yunhetong.net>
2018-03-02 15:17:04 +0800

    add eureka server

--- src/main/java/com/springcloud/ConfigClientApplication.java
+++ src/main/java/com/springcloud/ConfigClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
@@ -19,6 +21,8 @@ public class ConfigClientApplication {
 
 	@Value("${foo}")
 	String foo;
+
+
 	@RequestMapping(value = "/hi")
 	public String hi(){
 		return foo;
